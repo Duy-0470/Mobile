672f653b595a917f6dbee5ba1e8b3421d36f3825
Truongducthien <80139154+Truongducthien@users.noreply.github.com>
2022-06-12 03:07:47 +0700

    ui

---
+++ AppAmNhac/app/src/main/java/com/example/appamnhac/Activity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.appamnhac.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.appamnhac.Adapter.MainViewPaperAdapter;
+import com.example.appamnhac.Fragment.Fragment_Trang_Chu;
+import com.example.appamnhac.Fragment.Fragmet_Tim_Kiem;
+import com.example.appamnhac.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        anhxa();
+    }
+    private void init() {
+        MainViewPaperAdapter mainViewPaperAdapter = new MainViewPaperAdapter(getSupportFragmentManager());
+        mainViewPaperAdapter.addFragment(new Fragment_Trang_Chu(), "Trang chu");
+        mainViewPaperAdapter.addFragment(new Fragmet_Tim_Kiem(), "Tim kiem");
+        viewPager.setAdapter(mainViewPaperAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.icontrangchu);
+        tabLayout.getTabAt(1).setIcon(R.drawable.iconsearch);
+    }
+    private void anhxa() {
+        tabLayout = findViewById(R.id.myTabLayout);
+        viewPager = findViewById(R.id.myViewPager);
+    }
+}
---
+++ AppAmNhac/app/src/main/java/com/example/appamnhac/Fragment/Fragmet_Tim_Kiem.java
@@ -0,0 +1,22 @@
+package com.example.appamnhac.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.appamnhac.R;
+
+public class Fragmet_Tim_Kiem extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_tim_kiem, container,false);
+        return view;
+    }
+}

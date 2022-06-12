31466263954f6aca24a30c0e38f17783a04f88af
Hoàng Mai <91770764+dhmai18@users.noreply.github.com>
2022-06-12 15:10:28 +0700

    UI bai 10 (22')

--- AppAmNhac/app/src/main/java/com/example/appamnhac/Fragment/Fragment_Banner.java
+++ AppAmNhac/app/src/main/java/com/example/appamnhac/Fragment/Fragment_Banner.java
@@ -8,15 +8,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.appamnhac.R;
+import com.example.appamnhac.Service.APIService;
+import com.example.appamnhac.Service.Dataservice;
+
+import javax.security.auth.callback.Callback;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class Fragment_Banner extends Fragment {
     View view;
+    ViewPager viewPager;
+    CircleIndicator circleIndicator;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_banner,container,false);
+        anhxa();
         return view;
     }
+
+    private void anhxa(){
+        viewPager = view.findViewById(R.id.viewPager);
+        circleIndicator = view.findViewById(R.id.indicatordefault);
+    }
+
+    /*private void GetData() {
+        Dataservice dataservice = APIService.getService();
+        Call<List<Quangcao>> callback = dataservice.GetDataBanner();
+        callback.enqueue (new Callback<List<Quangcao>>() {
+
+        }
+    }*/
 }

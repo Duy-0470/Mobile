bc5058fff859b8f18eaf6b3b69e4bf7cc06753e5
Truongducthien <80139154+Truongducthien@users.noreply.github.com>
2022-06-12 19:28:56 +0700

    13 14

--- AppAmNhac/app/src/main/java/com/example/appamnhac/Fragment/Fragment_Playlist.java
+++ AppAmNhac/app/src/main/java/com/example/appamnhac/Fragment/Fragment_Playlist.java
@@ -6,13 +6,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.example.appamnhac.Adapter.PlaylistAdapter;
 import com.example.appamnhac.Model.Playlist;
 import com.example.appamnhac.R;
 import com.example.appamnhac.Service.APIService;
 import com.example.appamnhac.Service.Dataservice;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +28,17 @@ import retrofit2.Response;
 
 public class Fragment_Playlist extends Fragment {
     View view;
+    ListView lvplaylist;
+    TextView txttitleplaylist,txtviewxemthemplaylist;
+    PlaylistAdapter playlistAdapter;
+    ArrayList<Playlist> mangplaylist;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_playlist,container,false);
+        lvplaylist = view.findViewById(R.id.listviewplaylist);
+        txttitleplaylist = view.findViewById(R.id.textviewtitleplaylist);
+        txtviewxemthemplaylist = view.findViewById(R.id.textviewviewmoreplaylist);
         GetData();
         return view;
     }
@@ -37,8 +49,9 @@ public class Fragment_Playlist extends Fragment {
         callback.enqueue(new Callback<List<Playlist>>() {
             @Override
             public void onResponse(Call<List<Playlist>> call, Response<List<Playlist>> response) {
-                ArrayList<Playlist> mangplaylist = (ArrayList<Playlist>) response.body();
-                Log.d("BBB",mangplaylist.get(0).getTen());
+                mangplaylist = (ArrayList<Playlist>) response.body();
+              playlistAdapter = new PlaylistAdapter(getActivity(), android.R.layout.simple_list_item_1,mangplaylist);
+              lvplaylist.setAdapter(playlistAdapter);
             }
 
             @Override
